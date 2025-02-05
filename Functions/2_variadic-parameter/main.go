375cdcb0f0532149a984c721414dae21ecbf3b94
HasibAhmed99 <ahmedhasib737@gmail.com>
2025-02-06 03:09:12 +0600

    incomplete

---
+++ Functions/2_variadic-parameter/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+ x := sum(1, 2, 3, 4, 5, 6, 7, 8, 9)
+ fmt.Println("The sum is", x)
+}
+func sum(xi ...int) int {
+	fmt.Println(xi)
+	fmt.Printf("%T\n", xi)
+
+	n := 0
+	for _, v := range xi{
+		n += v,
+	}
+	return n 
+}
---
+++ Functions/3_unfurling-a-slice/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+    nx := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	x := sum(nx ...)
+	fmt.Println("The sum is", x)
+}
+
+func sum(xi ...int) int {
+	fmt.Println(xi)
+	fmt.Printf("%T\n", xi)
+
+	n := 0
+	for _, v := range xi {
+		 n += v
+	}
+	return n
+}
