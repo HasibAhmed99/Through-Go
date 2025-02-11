d96ba4ea090652b510f7e69b19abd933c873cdd3
HasibAhmed99 <ahmedhasib737@gmail.com>
2025-02-11 12:54:40 +0600

    not yet

---
+++ Functions/11_writer-interface-byte-buffer/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func main() {
+	b := bytes.NewBufferString("Hello ")
+	fmt.Println(b.String())
+	b.WriteString("Gophers!")
+	fmt.Println(b.String())
+	b.Reset()
+	b.WriteString("It's Tuesday ")
+	fmt.Println(b.String())
+	b.Write([]byte("Happy Happy"))
+	fmt.Println(b.String())
+}
