349f46aa6b12ff860cebac7a61d67033b49d3f71
HasibAhmed99 <ahmedhasib737@gmail.com>
2025-02-13 17:02:35 +0600

    not yet

--- Functions/16_returning-a-func/main.go
+++ Functions/16_returning-a-func/main.go
@@ -8,8 +8,11 @@ func main() {
 	//fmt.Println(foo()) // I can also print like this
 	
 	  y := bar()
-	 fmt.Println(y())
+	  fmt.Println(y())
 	// fmt.Println(bar()()) // This also works
+	fmt.Printf("%T\n", foo)
+	fmt.Printf("%T\n", bar)
+	fmt.Printf("%T\n", y)
 }
 
 func foo() int {
