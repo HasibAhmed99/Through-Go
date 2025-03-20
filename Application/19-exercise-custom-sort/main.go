dedaf2ade7ae66778408852723857907eaafec3f
HasibAhmed99 <ahmedhasib737@gmail.com>
2025-03-20 17:54:38 +0600

    six

---
+++ Application/19-exercise-custom-sort/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type user struct {
+	First   string
+	Last    string
+	Age     int
+	Sayings []string
+}
+
+type ByAge []user
+
+func (ba ByAge) Len() int           { return len(ba) }
+func (ba ByAge) Swap(i, j int)      { ba[i], ba[j] = ba[j], ba[i] }
+func (ba ByAge) Less(i, j int) bool { return ba[i].Age < ba[j].Age }
+
+type ByLast []user
+
+func (bl ByLast) Len() int           { return len(bl) }
+func (bl ByLast) Swap(i, j int)      { bl[i], bl[j] = bl[j], bl[i] }
+func (bl ByLast) Less(i, j int) bool { return bl[i].Last < bl[j].Last }
+
+func main() {
+	u1 := user{
+		First: "Harry",
+		Last:  "Potter",
+		Age:   44,
+		Sayings: []string{
+			"Shaken, not stirred",
+			"Youth is no guarantee of innovation",
+			"In his majesty's royal service",
+		},
+	}
+
+	u2 := user{
+		First: "Bob",
+		Last:  "Marley",
+		Age:   36,
+		Sayings: []string{
+			"James, it is soo good to see you",
+			"Would you like me to take care of that for you, James?",
+			"I would really prefer to be a secret agent myself.",
+		},
+	}
+
+	u3 := user{
+		First: "Peter",
+		Last:  "Parker",
+		Age:   36,
+		Sayings: []string{
+			"Oh, James. You didn't.",
+			"Dear God, what has James done now?",
+			"Can someone please tell me where James Bond is?",
+		},
+	}
+
+	people := []user{u1, u2, u3}
+	fmt.Println(people)
+	sort.Sort(ByAge(people))
+	fmt.Println(people)
+
+	sort.Sort(ByLast(people))
+	fmt.Println(people)
+
+	for _, v := range people
+
+}
