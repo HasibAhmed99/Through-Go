02955f76a9ca7897de8bd221e5b9e7b98fb64e6e
HasibAhmed99 <ahmedhasib737@gmail.com>
2025-05-02 20:16:34 +0600

    added another

--- Functions/46-fmt-vs-print-functions/main.go
+++ Functions/46-fmt-vs-print-functions/main.go
@@ -9,3 +9,9 @@ func main() {
 	// fmt.Println() – requires import "fmt"
 	fmt.Println("This is from fmt.Println (formatted and professional output)")
 }
+
+/*
+ Benefit Summary:
+Use fmt for any real application, logging, or formatted messages — it’s reliable, readable, and future-proof.
+Use println() only for quick and dirty debug logs during early development or experimentation.
+*/
